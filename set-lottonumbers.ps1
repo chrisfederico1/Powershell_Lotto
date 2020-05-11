0025ab315378355e34cadeb18fc3ffa54156241a
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2020-05-11 15:31:23 -0500

    Some Code

---
+++ set-lottonumbers.ps1
@@ -0,0 +1,35 @@
+
+# Show Menu
+
+# Clear Screen
+Clear-Host
+
+write-host "Pick which Game you want to play:"
+write-host "1. Mega Millions"
+write-host "2. PowerBall"
+
+
+# Mega Millions Numbers
+
+
+$First_Number = get-random -Minimum 1 -Maximum 70 -
+$Second_Number = get-random -Minimum 1 -Maximum 70
+$Third_Number = get-random -Minimum 1 -Maximum 70
+$Fourth_Number = get-random -Minimum 1 -Maximum 70
+$Fifth_Number = get-random -Minimum 1 -Maximum 70
+$MegaBall_Number = get-random -Minimum 1 -Maximum 25
+
+
+
+
+$First_Number
+$Second_Number
+$Third_Number
+$Fourth_Number
+$Fifth_Number
+$MegaBall_Number
+
+
+
+
+
