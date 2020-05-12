6929fdfb69e1b8eef62a1c979ef9665100bff8ab
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2020-05-12 11:11:41 -0500

    Added more code Functions

--- set-lottonumbers.ps1
+++ set-lottonumbers.ps1
@@ -1,4 +1,33 @@
 
+function Mega_First_5_Random_Num ()
+{
+    $n = get-random -Minimum 1 -Maximum 70
+    return $n
+}
+
+
+function PowerBall_First_5_Random_Num ()
+    {
+        $n = get-random -Minimum 1 -Maximum 69
+        return $n
+    }
+
+function Mega_Ball_Random_Num ()
+    {
+        $n = get-random -Minimum 1 -Maximum 25
+        return $n
+    
+    }
+
+function PowerBall_Random_Number ()
+    {
+        $n = get-random -Minimum 1 -Maximum 26
+        return $n
+    }
+
+
+
+
 # Show Menu
 
 # Clear Screen
@@ -8,26 +37,50 @@ write-host "Pick which Game you want to play:"
 write-host "1. Mega Millions"
 write-host "2. PowerBall"
 
+$game = read-host ">"
 
-# Mega Millions Numbers
+if ($game -match "1")
+    {
+        
+        # Mega Millions Numbers
+        $NUM_of_Tickets =Read-host "How many tickets do you want"
+
+        "";""
+
+        write-host "Mega Million Numbers"
+        write-host "---------------------"
+        
 
 
-$First_Number = get-random -Minimum 1 -Maximum 70 -
-$Second_Number = get-random -Minimum 1 -Maximum 70
-$Third_Number = get-random -Minimum 1 -Maximum 70
-$Fourth_Number = get-random -Minimum 1 -Maximum 70
-$Fifth_Number = get-random -Minimum 1 -Maximum 70
-$MegaBall_Number = get-random -Minimum 1 -Maximum 25
+        for ($i = 1;$i -le $NUM_of_Tickets; $i++)
+            {
+               write-host $(Mega_First_5_Random_Num) $(Mega_First_5_Random_Num) $(Mega_First_5_Random_Num) $(Mega_First_5_Random_Num) $(Mega_First_5_Random_Num) $(Mega_Ball_Random_Num)
+            }
+    }
+
+if ($game -match "2")
+    {
+        # PowerBall Numbers
+        $NUM_of_Tickets =Read-host "How many tickets do you want"
+        "";""
+        write-host "PowerBall Numbers"
+        write-host "-----------------"
+
+        for ($i = 1;$i -le $NUM_of_Tickets; $i++)
+            {
+                
+            write-host $(PowerBall_First_5_Random_Num) $(PowerBall_First_5_Random_Num) $(PowerBall_First_5_Random_Num) $(PowerBall_First_5_Random_Num) $(PowerBall_First_5_Random_Num) $(PowerBall_Random_Number)
+                
+            }
+    }
 
 
 
 
-$First_Number
-$Second_Number
-$Third_Number
-$Fourth_Number
-$Fifth_Number
-$MegaBall_Number
+
+
+
+
 
 
 
